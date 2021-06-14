f6a784c1214d8080eb6fb4a690ae3dacce47db4b
Tero Tikkanen <tero.tikkanen@gmail.com>
2021-06-14 23:27:14 +0300

    Make scenario changeable
    TODO: refetch data

--- components/general/ScenarioSelector.js
+++ components/general/ScenarioSelector.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useTranslation } from 'next-i18next';
-import { gql, useQuery, useReactiveVar } from '@apollo/client';
+import { gql, useQuery, useMutation } from '@apollo/client';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Spinner } from 'reactstrap';
 
 const GET_SCENARIOS = gql` 
@@ -14,8 +14,23 @@ const GET_SCENARIOS = gql`
   }
 `;
 
+const ACTIVATE_SCENARIO = gql` 
+  mutation ActivateScenario($scenarioId: ID!) {
+    activateScenario(id: $scenarioId) {
+      ok
+    }
+  }
+`;
+
 const ScenarioSelector = () => {
   const { t } = useTranslation();
+
+  const [activateScenario] = useMutation(ACTIVATE_SCENARIO, {
+    refetchQueries: [
+      { query: GET_SCENARIOS },
+    ],
+  });
+
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const toggle = () => setDropdownOpen((prevState) => !prevState);
@@ -33,8 +48,8 @@ const ScenarioSelector = () => {
 
   const activeScenario = scenarios?.find((scenario) => scenario.isActive)
     || scenarios?.find((scenario) => scenario.isDefault);
-  const displayScenario = `${t('scenario')}: ${activeScenario.name.length > 15
-    ? `${activeScenario.name.substring(0, 15)}&hellip;` : activeScenario.name}`;
+  const displayScenario = `${t('scenario')}: ${activeScenario.name.length > 20
+    ? `${activeScenario.name.substring(0, 20)}&hellip;` : activeScenario.name}`;
 
   return (
     <Dropdown isOpen={dropdownOpen} toggle={toggle}>
@@ -44,7 +59,13 @@ const ScenarioSelector = () => {
       <DropdownMenu>
         <DropdownItem header>{ t('change-scenario') }</DropdownItem>
         { scenarios?.map((scenario) => (
-          <DropdownItem key={scenario.id} active={scenario.isActive}>{ scenario.name }</DropdownItem>
+          <DropdownItem
+            key={scenario.id}
+            active={scenario.isActive}
+            onClick={(e) => activateScenario({ variables: { scenarioId: scenario.id } })}
+          >
+            { scenario.name }
+          </DropdownItem>
         ))}
       </DropdownMenu>
     </Dropdown>
