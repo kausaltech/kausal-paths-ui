d8016e313423fcbc9191c11f41facee348db943f
Tero Tikkanen <tero.tikkanen@gmail.com>
2021-06-15 21:16:21 +0300

    Make scenario change more global

--- components/general/ScenarioSelector.js
+++ components/general/ScenarioSelector.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useTranslation } from 'next-i18next';
-import { gql, useQuery, useMutation } from '@apollo/client';
+import { gql, useQuery, useMutation, useReactiveVar } from '@apollo/client';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Spinner } from 'reactstrap';
 import { activeScenarioVar } from 'common/cache';
 import { GET_HOME_PAGE } from 'common/queries/getHomePage';
@@ -11,6 +11,10 @@ const ACTIVATE_SCENARIO = gql`
   mutation ActivateScenario($scenarioId: ID!) {
     activateScenario(id: $scenarioId) {
       ok
+      activeScenario {
+        id
+        name
+      }
     }
   }
 `;
@@ -19,8 +23,17 @@ const ScenarioSelector = () => {
   const { t } = useTranslation();
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const toggle = () => setDropdownOpen((prevState) => !prevState);
+  const activeScenario = useReactiveVar(activeScenarioVar);
 
   const { loading, error, data } = useQuery(GET_SCENARIOS);
+  const [activateScenario, { loading: mutationLoading, error: mutationError }] = useMutation(ACTIVATE_SCENARIO, {
+    refetchQueries: [
+      { query: GET_ACTION_LIST },
+      { query: GET_SCENARIOS },
+      { query: GET_HOME_PAGE },
+    ],
+    onCompleted: (dat) => activeScenarioVar(dat.activateScenario.activeScenario),
+  });
 
   if (loading) {
     return <Spinner className="m-5" style={{ width: '3rem', height: '3rem' }} />;
@@ -31,28 +44,15 @@ const ScenarioSelector = () => {
 
   const scenarios = data?.scenarios;
 
-  const [activateScenario] = useMutation(ACTIVATE_SCENARIO, {
-    refetchQueries: [
-      { query: GET_ACTION_LIST },
-      { query: GET_SCENARIOS },
-      { query: GET_HOME_PAGE },
-    ],
-    // TODO: figure out which scenario got just activated... this one returns the previous active
-    onCompleted: (data) => activeScenarioVar(scenarios.find((scen) => scen.isActive)),
-  });
-
-  // activateScenario.refetchQueries(GET_SCENARIOS);
-  // activateScenario.refetchQueries(GET_HOME_PAGE);
-
-  const activeScenario = scenarios?.find((scenario) => scenario.isActive)
-    || scenarios?.find((scenario) => scenario.isDefault);
   const displayScenario = `${t('scenario')}: ${activeScenario.name.length > 20
     ? `${activeScenario.name.substring(0, 20)}&hellip;` : activeScenario.name}`;
 
   return (
     <Dropdown isOpen={dropdownOpen} toggle={toggle}>
       <DropdownToggle caret color="light">
-        <span dangerouslySetInnerHTML={{ __html: displayScenario }} />
+        { mutationLoading
+          ? <span>{ `${t('scenario')}: ${t('loading')}` }</span>
+          : <span dangerouslySetInnerHTML={{ __html: displayScenario }} /> }
       </DropdownToggle>
       <DropdownMenu>
         <DropdownItem header>{ t('change-scenario') }</DropdownItem>
@@ -60,7 +60,7 @@ const ScenarioSelector = () => {
           <DropdownItem
             key={scenario.id}
             active={scenario.isActive}
-            onClick={(e) => activateScenario({ variables: { scenarioId: scenario.id } })}
+            onClick={() => activateScenario({ variables: { scenarioId: scenario.id } })}
           >
             { scenario.name }
           </DropdownItem>
