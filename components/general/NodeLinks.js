7f3019c5f5baf96dd4ac44216c06948f17e9b4ac
Tero Tikkanen <tero.tikkanen@gmail.com>
2021-06-22 01:25:22 +0300

    Improve node page

---
+++ components/general/NodeLinks.js
@@ -0,0 +1,81 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+import * as Icon from 'react-bootstrap-icons';
+import { Container, Row, Col, ListGroup, ListGroupItem } from 'reactstrap';
+
+const InputNodes = styled.div`
+  margin-bottom: 4rem;
+
+  .list-group-item {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  svg {
+    margin-left: 1rem;
+  }
+`;
+
+const OutputNodes = styled.div`
+  margin-bottom: 4rem;
+  text-align: right;
+
+  .list-group-item {
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: space-between;
+    text-align: right;
+  }
+
+  svg {
+    margin-right: 1rem;
+  }
+`;
+
+const NodeLinks = (props) => {
+  const { inputNodes, outputNodes } = props;
+  return (
+    <Container>
+      <Row>
+        <Col md={{ size: 5 }}>
+          { inputNodes.length > 0 && (
+          <InputNodes>
+            <ListGroup>
+              <ListGroupItem tag="h5">
+                Tähän vaikuttaa
+              </ListGroupItem>
+              { inputNodes.map((inputNode, index) => (
+                <Link key={inputNode.id} href={`/node/${inputNode.id}`}>
+                  <ListGroupItem tag="a" href="#">
+                    <span>{ inputNode.name }</span>
+                  </ListGroupItem>
+                </Link>
+              ))}
+            </ListGroup>
+          </InputNodes>
+          )}
+        </Col>
+        <Col md={{ size: 5, offset: 2 }}>
+          { outputNodes.length > 0 && (
+          <OutputNodes>
+            <ListGroupItem tag="h5">
+              Tämä vaikuttaa
+            </ListGroupItem>
+            <ListGroup>
+              { outputNodes.map((outputNode, index) => (
+                <Link key={outputNode.id} href={`/node/${outputNode.id}`}>
+                  <ListGroupItem tag="a" href="#">
+                    <span>{ outputNode.name }</span>
+                  </ListGroupItem>
+                </Link>
+              ))}
+            </ListGroup>
+          </OutputNodes>
+          )}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default NodeLinks;
--- pages/node/[slug].js
+++ pages/node/[slug].js
@@ -1,19 +1,21 @@
 import { useEffect } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import Link from 'next/link';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import { gql, useQuery, useReactiveVar } from '@apollo/client';
-import { Spinner, Container } from 'reactstrap';
+import { Spinner, Container, Row, Col } from 'reactstrap';
 import styled from 'styled-components';
-import { activeScenarioVar, settingsVar } from 'common/cache';
+import { activeScenarioVar, settingsVar, yearRangeVar } from 'common/cache';
 import Layout from 'components/Layout';
 import SettingsPanel from 'components/general/SettingsPanel';
 import NodePlot from 'components/general/NodePlot';
+import DashCard from 'components/general/DashCard';
+import NodeLinks from 'components/general/NodeLinks';
 
 const HeaderSection = styled.div`
-  padding: 3rem 0 1rem; 
-  background-color: ${(props) => props.theme.graphColors.grey020};
+  padding: 3rem 0 1rem;
+  margin-bottom: 8rem;
+  background-color: ${(props) => props.theme.graphColors.grey050};
 `;
 
 const PageHeader = styled.div` 
@@ -22,6 +24,29 @@ const PageHeader = styled.div`
   h1 {
     font-size: 1.5rem;
     color: ${(props) => props.theme.themeColors.dark};
+  }
+`;
+
+const HeaderCard = styled.div` 
+  margin: 3rem 0 -8rem;
+  padding: 2rem;
+  border-radius: 1rem;
+  background-color: ${(props) => props.theme.themeColors.white};
+`;
+
+const ActionDescription = styled.div`
+  margin-bottom: 2rem;
+  font-size: 1.15rem;
+`;
+
+const ContentWrapper = styled.div`
+  padding: 1rem;
+  margin: .5rem 0;
+  background-color: ${(props) => props.theme.graphColors.grey005};
+  border-radius: 10px;
+
+  .x2sstick text, .xtick text {
+    text-anchor: end !important;
   }
 `;
 
@@ -87,6 +112,8 @@ query GetNodePage($node: ID!) {
 export default function NodePage() {
   const router = useRouter();
   const { slug } = router.query;
+  // const { t } = useTranslation();
+  const yearRange = useReactiveVar(yearRangeVar);
 
   const { loading, error, data, refetch } = useQuery(GET_NODE_PAGE_CONTENT, {
     variables: {
@@ -117,50 +144,45 @@ export default function NodePage() {
       <HeaderSection>
         <Container>
           <PageHeader>
-            <h1>
-              {node.name}
-            </h1>
-            <div dangerouslySetInnerHTML={{ __html: node.description }} />
-            <NodePlot
-              metric={node.metric}
-              startYear={settingsVar().minYear}
-              endYear={settingsVar().maxYear}
-              color={node.color}
-            />
+            <HeaderCard>
+              <h1>
+                {node.name}
+              </h1>
+              <ActionDescription dangerouslySetInnerHTML={{ __html: node.description }} />
+              { node.metric && (
+              <ContentWrapper>
+                <NodePlot
+                  metric={node.metric}
+                  impactMetric={node.impactMetric}
+                  year="2021"
+                  startYear={yearRange[0]}
+                  endYear={yearRange[1]}
+                  color={node.color}
+                  isAction={node.isAction}
+                />
+              </ContentWrapper>
+              )}
+            </HeaderCard>
           </PageHeader>
         </Container>
       </HeaderSection>
       <Container>
-        { node.inputNodes.length > 0 && (
-        <>
-          <h2>Tähän vaikuttaa</h2>
-          <ul>
-            { node.inputNodes.map((inputNode, index) => (
-              <li key={inputNode.id}>
-                <Link href={`/${inputNode.isAction ? 'actions' : 'node'}/${inputNode.id}`}>
-                  <a>
-                    { inputNode.name }
-                  </a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </>
-        )}
-        { node.outputNodes.length > 0 && (
-        <>
-          <h2>Tämä vaikuttaa</h2>
-          <ul>
-            { node.outputNodes.map((outputNode, index) => (
-              <li key={outputNode.id}><Link href={`/node/${outputNode.id}`}><a>{ outputNode.name }</a></Link></li>
-            ))}
-          </ul>
-        </>
-        )}
+        <NodeLinks
+          outputNodes={node.outputNodes}
+          inputNodes={node.inputNodes}
+        />
       </Container>
+      { node.body && (
       <Container>
-        <div dangerouslySetInnerHTML={{ __html: node.body }} />
+        <Row>
+          <Col md={{ size: 8, offset: 2 }}>
+            <DashCard>
+              <div dangerouslySetInnerHTML={{ __html: node.body }} />
+            </DashCard>
+          </Col>
+        </Row>
       </Container>
+      )}
       <SettingsPanel
         defaultYearRange={[settingsVar().latestMetricYear, settingsVar().maxYear]}
       />
