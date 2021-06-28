ce1172150c9ca0a8760756b7a0a1d9e175a5d27b
Tero Tikkanen <tero.tikkanen@gmail.com>
2021-06-28 22:25:10 +0300

    Handle cumulative impact 0

--- components/general/ImpactDisplay.js
+++ components/general/ImpactDisplay.js
@@ -42,7 +42,7 @@ const ImpactDisplay = (props) => {
       <ImpactDisplayHeader muted={muted}>
         { t('impact') }
       </ImpactDisplayHeader>
-      { effectCumulative && (
+      { effectCumulative !== undefined && (
       <ImpactDisplayItem>
         <HighlightValue
           displayValue={`${cumulativePrefix}${beautifyValue(effectCumulative || 0)}`}
