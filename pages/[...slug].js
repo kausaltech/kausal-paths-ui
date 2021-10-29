7523d9caced1af7fce63d1ebf53c7ecc50b3714e
Juha Yrjölä <juha.yrjola@iki.fi>
2021-10-29 16:49:06 +0300

    Add a catch-all page

---
+++ pages/[...slug].js
@@ -0,0 +1,11 @@
+import Page from 'components/pages/Page';
+import { useRouter } from 'next/router'
+import { useSite } from 'context/site';
+
+
+export default function SlugPage(props) {
+  const router = useRouter()
+  const { slug } = router.query;
+  const path = '/' + slug.join('/');
+  return <Page path={path} />;
+}
