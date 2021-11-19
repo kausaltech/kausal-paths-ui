83f754abbf773556dd7fcd982c5b784a3d212fe7
Bernhard Bliem <bernhard@bliem.xyz>
2021-11-19 15:57:20 +0100

    Set up Sentry using its wizard

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const i18n = {
 };
 
 const path = require('path')
+const { withSentryConfig } = require('@sentry/nextjs');
 
 const DEFAULT_GRAPHQL_API_URL = process.env.DEFAULT_GRAPHQL_API_URL || 'https://api.paths.kausal.tech/v1/graphql/';
 const INSTANCE_IDENTIFIER = process.env.INSTANCE_IDENTIFIER;
@@ -37,4 +38,18 @@ const nextConfig = {
   i18n,
 }
 
-module.exports = nextConfig;
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
