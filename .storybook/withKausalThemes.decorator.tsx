426b635ac076f20f838e07d5593157478d81b56d
Tero Tikkanen <tero.tikkanen@gmail.com>
2024-08-07 18:38:30 +0300

    Config storybook themes

---
+++ .storybook/withKausalThemes.decorator.tsx
@@ -0,0 +1,28 @@
+import { DecoratorHelpers } from '@storybook/addon-themes';
+import { ThemeProvider } from 'styled-components';
+
+const { initializeThemeState, pluckThemeFromContext, useThemeParameters } = DecoratorHelpers;
+
+export const withKausalThemes = ({ themes, defaultTheme }) => {
+  initializeThemeState(Object.keys(themes), defaultTheme);
+
+  return (story, context) => {
+    const selectedTheme = pluckThemeFromContext(context);
+    const { themeOverride } = useThemeParameters();
+
+    const selected = themeOverride || selectedTheme || defaultTheme;
+    // Add full theme object to args for use in story
+    Object.assign(context.args, { activeTheme: themes[selected] });
+
+    return (
+      <>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href={`/static/themes/${themes[selected].name}/main.css`}
+        />
+        <ThemeProvider theme={themes[selected]}>{story(context)}</ThemeProvider>
+      </>
+    );
+  };
+};
