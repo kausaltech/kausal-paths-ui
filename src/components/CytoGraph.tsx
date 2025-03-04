0371ded41a907bb123474fc912d14846352bcecc
Tero Tikkanen <tero.tikkanen@gmail.com>
2025-03-04 22:30:19 +0200

    Use react-cytograph

--- src/components/CytoGraph.tsx
+++ src/components/CytoGraph.tsx
@@ -9,22 +9,51 @@ import cytoscape, {
   type ElementDefinition,
   type NodeDefinition,
 } from 'cytoscape';
+import CytoscapeComponent from 'react-cytoscapejs';
 import dagre, { type DagreLayoutOptions } from 'cytoscape-dagre';
+// @ts-ignore
 import cytoscapeNodeHtmlLabel from 'cytoscape-node-html-label';
 import { getContrast } from 'polished';
 import styled, { useTheme } from 'styled-components';
 
 import SelectDropdown from './common/SelectDropdown';
 
+const GraphContainer = styled.div`
+  width: 100%;
+  height: 800px;
+  position: relative;
+`;
+
 cytoscape.use(dagre);
 cytoscapeNodeHtmlLabel(cytoscape);
 
-const VisContainer = styled.div`
-  width: 100%;
-  height: 100vh;
-  background-color: #f6f6f6;
-  margin: 0;
-`;
+function getBackgroundColor(node: GetCytoscapeNodesQuery['nodes'][0]) {
+  const nodeColors = {
+    action: '#94dd94',
+    emissions: '#c6b58a',
+    utility: '#93c2d6',
+    effect: '#e0b472',
+    currency: '#748d77',
+    unit_price: '#9ecae1',
+    energy: '#b74848',
+    emission_factor: '#d8e899',
+    argument: '#ff7354',
+  };
+
+  if (node.__typename == 'ActionNode') {
+    if (node.group?.color) {
+      const actionColor = chroma(node.group.color).brighten().hex();
+      return actionColor;
+    }
+    return nodeColors.action;
+  }
+  if (node.color) {
+    return node.color;
+  } else if (node.quantity && nodeColors[node.quantity]) {
+    return nodeColors[node.quantity];
+  }
+  return '#eeeeee';
+}
 
 function wordWrap(inputStr, maxWidth, newLineStr = '\n') {
   let done = false;
@@ -85,6 +114,51 @@ function NodeSelector(props: NodeSelectorProps) {
   );
 }
 
+const cyLayoutOptions: DagreLayoutOptions = {
+  name: 'dagre',
+  ranker: 'tight-tree',
+  edgeWeight: (edge) => (edge.data('type') === 'parent' ? 5 : 1),
+  nodeDimensionsIncludeLabels: true,
+  animate: false,
+  rankDir: 'TB',
+  animationDuration: 2000,
+};
+
+const edgeStyle: cytoscape.Css.Edge = {
+  label: 'data(label)',
+  'target-text-offset': 1,
+  'target-arrow-shape': 'triangle',
+  'target-arrow-color': 'data(color)',
+  'arrow-scale': 2,
+  'line-color': 'data(color)',
+  'text-outline-width': 3,
+  'text-outline-color': 'data(color)',
+  color: '#ffffff',
+  'curve-style': 'bezier',
+  'font-size': '18px',
+  'font-weight': 'bold',
+  width: 2,
+};
+
+const nodeStyle: cytoscape.Css.Node = {
+  shape: 'rectangle',
+  'background-color': 'data(backgroundColor)',
+  //color: 'data(textColor)',
+  color: '#cccccc',
+  'text-valign': 'center',
+  padding: '24px',
+  label: 'data(label)',
+  'text-opacity': 0,
+  'text-wrap': 'wrap',
+  'text-outline-width': 0,
+  'font-weight': 'normal',
+  'border-width': (node) => (node.data('type') === 'action' ? '2px' : '0px'),
+  'border-color': '#000000',
+  'border-style': 'dashed',
+  width: 'label',
+  height: 'label',
+};
+
 type CytoGraphProps = {
   nodes: GetCytoscapeNodesQuery['nodes'];
 };
@@ -95,243 +169,113 @@ export default function CytoGraph(props: CytoGraphProps) {
   const router = useRouter();
   const theme = useTheme();
   const visRef = useRef<HTMLDivElement>(null);
+  const elements: ElementDefinition[] = [];
 
-  useEffect(() => {
-    const visNode = visRef.current;
-    if (!visNode) return;
-    const elements: ElementDefinition[] = [];
+  // Nodes
+  nodes.forEach((node) => {
+    const label = wordWrap(node.name, 30);
+    const hist = 'xxx';
+    const bgColor = getBackgroundColor(node);
+    const textColor = '#000000';
 
-    // Nodes
-    nodes.forEach((node) => {
-      function getBackgroundColor() {
-        if (node.__typename == 'ActionNode') {
-          if (node.group?.color) {
-            const actionColor = chroma(node.group.color).brighten().hex();
-            return actionColor;
-          }
-          return '#00ff00';
-        }
-        if (node.quantity == 'emissions') {
-          if (node.color) {
-            return node.color;
-          }
-          return '#aaaaaa';
-        }
-        if (node.quantity == 'energy') {
-          return '#ff0000';
-        }
-        if (node.quantity == 'emission_factor') {
-          return '#ffff00';
-        }
-        if (node.quantity == 'unit_price') {
-          if (node.color) {
-            return node.color;
-          }
-          return '#9ecae1';
-        }
-        if (node.quantity == 'currency') {
-          if (node.color) {
-            return node.color;
-          }
-          return '#3182bd';
-        }
-        if (node.quantity == 'effect') {
-          if (node.color) {
-            return node.color;
-          }
-          return '#fee8c8';
-        }
-        if (node.quantity == 'utility') {
-          if (node.color) {
-            return node.color;
-          }
-          return '#43a2ca';
-        }
-        if (node.quantity == 'argument') {
-          if (node.color) {
-            return node.color;
-          }
-          return '#ff7354';
-        }
-        return '#eeeeee';
-      }
-      let label = wordWrap(node.name, 30);
-      const latestHistorical = node.metric?.historicalValues?.[0];
-      let hist;
-      if (latestHistorical) {
-        const val = latestHistorical.value;
-        hist = {
-          year: latestHistorical.year,
-          value: val < 0 ? val.toPrecision(3) : val.toFixed(0),
-          unit: node.unit?.htmlShort,
-        };
-        label += `\n${hist.year}: ${hist.value} ${hist.unit}`;
-      }
-      const bgColor = getBackgroundColor();
-      let textColor = '#000000';
-      const whiteContrast = getContrast(bgColor, textColor);
-      if (whiteContrast < 8) textColor = '#ffffff';
+    const element: NodeDefinition = {
+      group: 'nodes',
+      data: {
+        id: node.id,
+        backgroundColor: bgColor,
+        textColor,
+        name: node.name,
+        hist,
+        label: label,
+        type: node.__typename == 'ActionNode' ? 'action' : 'node',
+      },
+    };
+    elements.push(element);
+  });
 
-      const element: NodeDefinition = {
-        group: 'nodes',
+  // Edges
+  nodes.forEach((node) => {
+    node.outputNodes.forEach((target) => {
+      const edge: EdgeDefinition = {
+        group: 'edges',
         data: {
-          id: node.id,
-          backgroundColor: bgColor,
-          textColor,
-          name: node.name,
-          hist,
-          label: label,
-          type: node.__typename == 'ActionNode' ? 'action' : 'node',
+          id: `${node.id}-${target.id}`,
+          source: node.id,
+          target: target.id,
+          label: '',
+          color: 'teal',
         },
       };
-      elements.push(element);
+      elements.push(edge);
     });
-
-    // Edges
-    nodes.forEach((node) => {
-      node.outputNodes.forEach((target) => {
+    if (node.__typename == 'ActionNode') {
+      if (node.subactions.length) {
+        console.log(node);
+        console.log(node.subactions);
+      }
+      node.subactions.forEach((sub) => {
         const edge: EdgeDefinition = {
           group: 'edges',
           data: {
+            id: `${node.id}-${sub.id}`,
             source: node.id,
-            target: target.id,
+            target: sub.id,
             label: '',
             color: 'teal',
+            type: 'parent',
           },
         };
         elements.push(edge);
       });
-      if (node.__typename == 'ActionNode') {
-        if (node.subactions.length) {
-          console.log(node);
-          console.log(node.subactions);
-        }
-        node.subactions.forEach((sub) => {
-          const edge: EdgeDefinition = {
-            group: 'edges',
-            data: {
-              source: node.id,
-              target: sub.id,
-              label: '',
-              color: 'teal',
-              type: 'parent',
-            },
-          };
-          elements.push(edge);
-        });
-      }
-    });
-
-    const cyLayoutOptions: DagreLayoutOptions = {
-      name: 'dagre',
-      ranker: 'tight-tree',
-      edgeWeight: (edge) => (edge.data('type') === 'parent' ? 5 : 1),
-      nodeDimensionsIncludeLabels: true,
-      animate: false,
-      rankDir: 'TB',
-      animationDuration: 2000,
-    };
-
-    const edgeStyle: cytoscape.Css.Edge = {
-      label: 'data(label)',
-      'target-text-offset': 1,
-      'target-arrow-shape': 'triangle',
-      'target-arrow-color': 'data(color)',
-      'arrow-scale': 2,
-      'line-color': 'data(color)',
-      'text-outline-width': 3,
-      'text-outline-color': 'data(color)',
-      color: '#ffffff',
-      'curve-style': 'bezier',
-      'font-size': '18px',
-      'font-weight': theme.fontWeightBold,
-      width: 2,
-    };
-
-    const nodeStyle: cytoscape.Css.Node = {
-      shape: 'rectangle',
-      'background-color': 'data(backgroundColor)',
-      //color: 'data(textColor)',
-      color: '#cccccc',
-      width: 'label',
-      height: 'label',
-      'text-valign': 'center',
-      padding: '24px',
-      label: 'data(label)',
-      'text-opacity': 0,
-      'text-wrap': 'wrap',
-      'text-outline-width': 0,
-      'font-weight': theme.fontWeightNormal,
-      'border-width': (node) => (node.data('type') === 'action' ? '2px' : '0px'),
-      'border-color': '#000000',
-      'border-style': 'dashed',
-    };
-
-    const cy: cytoscape.Core = cytoscape({
-      container: visNode,
-      elements,
-      layout: cyLayoutOptions,
-      zoomingEnabled: true,
-      maxZoom: 2,
-      minZoom: 0.1,
-      style: [
-        // the stylesheet for the graph
-        {
-          selector: '*',
-          style: {
-            'font-family': `${theme.fontFamily}, sans-serif`,
-          },
-        },
-        {
-          selector: 'node',
-          style: nodeStyle,
-        },
-        {
-          selector: 'edge',
-          style: edgeStyle,
-        },
-      ],
-    });
-
-    if (selectedNode) {
-      const node = cy.getElementById(selectedNode);
-      const keepNodes = node.predecessors().union(node.successors()).add(node);
-      const removeNodes = cy.nodes().difference(keepNodes);
-      removeNodes.remove();
-      cy.layout(cyLayoutOptions).run();
-      cy.center(node);
-    } else {
-      cy.layout(cyLayoutOptions).run();
-      cy.center();
     }
+  });
 
-    cy.nodes().on('click', (e) => {
-      const clickedNode = e.target;
-      const nodeId = clickedNode.data('id');
-      router.push(`/node/${nodeId}`);
-    });
-    cy.nodeHtmlLabel([
-      {
-        query: 'node',
-        valign: 'center',
-        halign: 'center',
-        valignBox: 'center',
-        halignBox: 'center',
-        tpl: (data) => {
-          const name = wordWrap(data.name, 30, '<br />');
-          const histStr = data.hist
-            ? `<br />${data.hist.year}: <em>${data.hist.value} ${data.hist.unit}</em>`
-            : '';
-          return `<div style="color: ${data.textColor}"><strong>${name}</strong>${histStr}</div>`;
-        },
+  const cyStyle = [
+    // the stylesheet for the graph
+    {
+      selector: '*',
+      style: {
+        'font-family': `${theme.fontFamily}, sans-serif`,
       },
-    ]);
-  }, [visRef, nodes, router, selectedNode]);
+    },
+    {
+      selector: 'node',
+      style: nodeStyle,
+    },
+    {
+      selector: 'edge',
+      style: edgeStyle,
+    },
+  ];
 
+  console.log('ELEMENTS', elements);
   return (
-    <>
+    <GraphContainer>
       <NodeSelector nodes={nodes} selectedNode={selectedNode} setSelectedNode={setSelectedNode} />
-      <VisContainer ref={visRef} />
-    </>
+      <CytoscapeComponent
+        elements={elements}
+        style={{ width: '100%', height: '100%' }}
+        stylesheet={cyStyle}
+        layout={cyLayoutOptions}
+        cy={(cy) => {
+          cy.nodeHtmlLabel([
+            {
+              query: 'node',
+              valign: 'center',
+              halign: 'center',
+              valignBox: 'center',
+              halignBox: 'center',
+              tpl: (data) => {
+                const name = wordWrap(data.name, 30, '<br />');
+                const histStr = data.hist
+                  ? `<br />${data.hist.year}: <em>${data.hist.value} ${data.hist.unit}</em>`
+                  : '';
+                return `<div style="color: ${data.textColor}"><strong>${name}</strong>${histStr}</div>`;
+              },
+            },
+          ]);
+        }}
+      />
+    </GraphContainer>
   );
 }
