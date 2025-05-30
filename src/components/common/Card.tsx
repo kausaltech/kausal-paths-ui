b07e80e0a7455a95c21169eabcd3943757291e5c
Joe Smallwood <joe.smallwood@kausal.tech>
2025-05-30 13:49:29 +0300

    Support multiple action outcome nodes

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -1,4 +1,5 @@
-import { Card as ReactStrapCard, CardProps } from 'reactstrap';
+import { forwardRef } from 'react';
+import { Card as ReactStrapCard, type CardProps } from 'reactstrap';
 import styled from 'styled-components';
 
 const StyledCard = styled(ReactStrapCard)`
@@ -10,4 +11,10 @@ const StyledCard = styled(ReactStrapCard)`
 /**
  * A simple wrapper around Reactstrap's Card to support themed styles
  */
-export const Card = (props: CardProps) => <StyledCard {...props} />;
+export const Card = forwardRef((props: CardProps, ref) => (
+  <StyledCard {...props} innerRef={ref}>
+    {props.children}
+  </StyledCard>
+));
+
+Card.displayName = 'Card';
