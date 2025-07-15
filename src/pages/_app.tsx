39f8e4696fa5de8dc3872f710586ae854c83f7c6
Joe Smallwood <joe.smallwood@kausal.tech>
2025-07-15 11:22:48 +0300

    Add mui and multi-tenant theming

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react';
 
 import App, { type AppContext, type AppProps } from 'next/app';
 
-import type { ApolloClient } from '@apollo/client';
-import { ApolloProvider } from '@apollo/client';
+
+import { AppCacheProvider } from '@mui/material-nextjs/v14-pagesRouter';
+import type { ApolloClient} from '@apollo/client';
+import { ApolloProvider, isApolloError } from '@apollo/client';
 import type { Theme } from '@kausal/themes/types';
 import { AppCacheProvider } from '@mui/material-nextjs/v14-pagesRouter';
 import { ThemeProvider } from '@mui/material/styles';
@@ -45,7 +47,10 @@ import Layout from '@/components/Layout';
 import LocalizedNumbersContext, { createNumbersContext } from '@/context/numbers';
 import SiteContext, { type SiteContextType, type SiteI18nConfig } from '@/context/site';
 
-import '../../styles/default/main.scss';
+import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
+import { initializeMuiTheme } from '@/common/mui-theme/theme';
+
+require('../../styles/default/main.scss');
 
 type WatchLink = { title: string; url: string } | null;
 type DemoPage = { id: string; lang: string; title: string; urlPath: string };
@@ -229,17 +234,24 @@ function PathsApp(props: PathsAppProps) {
     yearRangeVar(yearRange);
   }
   const apolloClient = initializeApollo(null, siteContext.apolloConfig);
+  const muiTheme = initializeMuiTheme(themeProps);
 
   return (
     <AppCacheProvider {...props}>
       <SiteContext.Provider value={[siteContext, setSiteContext]}>
         <InstanceContext.Provider value={instanceContext}>
           <ApolloProvider client={apolloClient}>
-            <ThemeProvider theme={muiTheme}>
-              <ThemedGlobalStyles />
-              <LocalizedNumbersContext.Provider value={numbersContext}>
-                <Layout>{component}</Layout>
-              </LocalizedNumbersContext.Provider>
+            <ThemeProvider theme={themeProps}>
+              <MuiThemeProvider theme={muiTheme}>
+                <LocalizedNumbersContext.Provider value={numbersContext}>
+                  <ThemedGlobalStyles />
+                  <Layout>
+                    <Sentry.ErrorBoundary showDialog={!isProd} fallback={renderFallbackError}>
+                      {component}
+                    </Sentry.ErrorBoundary>
+                  </Layout>
+                </LocalizedNumbersContext.Provider>
+              </MuiThemeProvider>
             </ThemeProvider>
           </ApolloProvider>
         </InstanceContext.Provider>
