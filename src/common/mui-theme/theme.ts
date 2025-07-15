ebeccd4fd9f3bda30aa5682d8df7a2849b49b474
Joe Smallwood <joe.smallwood@kausal.tech>
2025-07-15 13:41:48 +0300

    Add base theme to emotion theme

--- src/common/mui-theme/theme.ts
+++ src/common/mui-theme/theme.ts
@@ -1,4 +1,4 @@
-import type { Theme } from '@kausal/themes/types';
+import type { Theme as BaseTheme } from '@kausal/themes/types';
 import { type Theme as MuiTheme, createTheme } from '@mui/material/styles';
 
 import { getComponents } from './components';
@@ -6,15 +6,24 @@ import { getPalette } from './palette';
 import { getTypography } from './typography';
 
 const isServer = typeof window === 'undefined';
+declare module '@mui/material/styles' {
+  interface Theme {
+    base: BaseTheme;
+  }
+  interface ThemeOptions {
+    base: BaseTheme;
+  }
+}
 
 let muiTheme: MuiTheme;
 
-export function initializeMuiTheme(theme: Theme) {
+export function initializeMuiTheme(theme: BaseTheme): MuiTheme {
   if (muiTheme && !isServer) {
     return muiTheme;
   }
 
   muiTheme = createTheme({
+    base: theme,
     palette: getPalette(theme),
     typography: getTypography(theme),
     shape: {
