61405fe876d7f801d220a8ac6b67bf9035f7aaf4
Tero Tikkanen <tero.tikkanen@gmail.com>
2025-07-15 17:44:26 +0300

    Make space for the legend in echarts component

--- src/components/charts/Chart.tsx
+++ src/components/charts/Chart.tsx
@@ -1,30 +1,28 @@
 // Import the echarts core module, which provides the necessary interfaces for using echarts.
-import * as echarts from 'echarts/core';
+import { useEffect, useRef } from 'react';
 
 // Import used charts
 import { BarChart, CustomChart, LineChart } from 'echarts/charts';
-
 // Import the tooltip, title, rectangular coordinate system, dataset and transform components
 import {
+  DatasetComponent,
+  GraphicComponent,
+  GridComponent,
+  LegendComponent,
+  MarkAreaComponent,
   TitleComponent,
   TooltipComponent,
-  GridComponent,
-  DatasetComponent,
   TransformComponent,
-  LegendComponent,
-  GraphicComponent,
 } from 'echarts/components';
-
+import * as echarts from 'echarts/core';
 // Features like Universal Transition and Label Layout
 import { LabelLayout, UniversalTransition } from 'echarts/features';
-
 // Import the Canvas renderer
 // Note that including the CanvasRenderer or SVGRenderer is a required step
 import { CanvasRenderer } from 'echarts/renderers';
-
 import throttle from 'lodash/throttle';
-import { useEffect, useRef } from 'react';
 import styled, { useTheme } from 'styled-components';
+
 import { getChartTheme } from './chart-theme';
 
 // Register the required components
@@ -42,11 +40,30 @@ echarts.use([
   CanvasRenderer,
   GraphicComponent,
   LineChart,
+  MarkAreaComponent,
 ]);
 
 const StyledChartWrapper = styled.div<{ $height?: string }>`
   height: ${({ $height }) => $height || '400px'};
 `;
+
+// Hack to add margin on the chart to fit the legend
+// Based on https://github.com/apache/echarts/issues/15654
+// Assumes that the legend is at the bottom of the chart
+const resizeLegend = (chart: echarts.ECharts) => {
+  if (chart) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-explicit-any
+    const found = (chart as any)._componentsViews.find(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+      (entry: any) => entry.type === 'legend.plain'
+    );
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+    const myLegendHeight: number = found?._backgroundEl?.shape?.height || 0;
+    chart.setOption({
+      grid: { bottom: myLegendHeight + 48 },
+    });
+  }
+};
 
 type Props = {
   isLoading: boolean;
@@ -61,15 +78,23 @@ export function Chart({ isLoading, data, height, onZrClick, className }: Props) 
   const chartRef = useRef<echarts.ECharts | null>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
 
+  // Initialize the chart
   useEffect(() => {
     const chart = echarts.init(wrapperRef.current, getChartTheme(theme).theme);
 
     chartRef.current = chart;
 
-    const throttledResize = throttle(() => chart.resize(), 1000, {
-      leading: false,
-      trailing: true,
-    });
+    const throttledResize = throttle(
+      () => {
+        chart.resize();
+        resizeLegend(chart);
+      },
+      1000,
+      {
+        leading: false,
+        trailing: true,
+      }
+    );
 
     window.addEventListener('resize', throttledResize);
 
@@ -81,6 +106,7 @@ export function Chart({ isLoading, data, height, onZrClick, className }: Props) 
     };
   }, [theme]);
 
+  // Show/hide the loading indicator
   useEffect(() => {
     if (chartRef.current) {
       if (isLoading) {
@@ -91,18 +117,21 @@ export function Chart({ isLoading, data, height, onZrClick, className }: Props) 
     }
   }, [isLoading]);
 
+  // Update the chart when the data changes
   useEffect(() => {
     if (chartRef.current && data) {
       chartRef.current.setOption(data);
+      resizeLegend(chartRef.current);
     }
   }, [data]);
 
+  // Add click handler to the chart
   useEffect(() => {
     const chart = chartRef.current;
     const chartZr = chart?.getZr();
     const withClickHandler = typeof onZrClick === 'function';
 
-    function handleClick(params) {
+    function handleClick(params: { offsetX: number; offsetY: number }) {
       if (chart && withClickHandler) {
         const pointInPixel = [params.offsetX, params.offsetY];
         const pointInGrid = chart.convertFromPixel('grid', pointInPixel);
