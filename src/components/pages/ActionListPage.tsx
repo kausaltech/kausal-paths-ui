2beb98753545684953b4720de983bfeaa1703ab1
Tero Tikkanen <tero.tikkanen@gmail.com>
2026-04-08 12:01:52 +0300

    Replace new separated components on ActionListPage

--- src/components/pages/ActionListPage.tsx
+++ src/components/pages/ActionListPage.tsx
@@ -1,124 +1,43 @@
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 
-import { type QueryResult, useQuery, useReactiveVar } from '@apollo/client';
+import { useQuery, useReactiveVar } from '@apollo/client';
 import styled from '@emotion/styled';
-import {
-  Box,
-  Container,
-  FormControl,
-  FormLabel,
-  Grid,
-  MenuItem,
-  Select,
-  ToggleButton,
-  ToggleButtonGroup,
-} from '@mui/material';
+import { Box, Container, FormControl, FormLabel, MenuItem, Select } from '@mui/material';
 
 import {
   type ActionListQuery,
   type ActionListQueryVariables,
-  DecisionLevel,
-  type ImpactOverviewsQuery,
   type PageQuery,
 } from '@/common/__generated__/graphql';
 import { activeGoalVar, yearRangeVar } from '@/common/cache';
 import type { TFunction } from '@/common/i18n';
 import { useTranslation } from '@/common/i18n';
 import { useInstance } from '@/common/instance';
-import { summarizeYearlyValuesBetween } from '@/common/preprocess';
-import ContentLoader from '@/components/common/ContentLoader';
 import GraphQLError from '@/components/common/GraphQLError';
 import { PageHero } from '@/components/common/PageHero';
 import Icon from '@/components/common/icon';
-import ActionsComparison from '@/components/general/ActionsComparison';
+import ActionListFilters from '@/components/general/ActionListFilters';
+import { ActionListGraphView } from '@/components/general/ActionListGraphView';
 import ActionsList from '@/components/general/ActionsList';
-import ActionsMac from '@/components/general/ActionsMac';
-import { CostBenefitAnalysis } from '@/components/general/CostBenefitAnalysis';
-import { ReturnOnInvestment } from '@/components/general/ReturnOnInvestment';
 import { GET_ACTION_LIST } from '@/queries/getActionList';
-import { GET_IMPACT_OVERVIEWS } from '@/queries/getImpactOverviews';
-import type { ActionWithEfficiency, SortActionsBy, SortActionsConfig } from '@/types/actions.types';
+import type { SortActionsBy, SortActionsConfig } from '@/types/actions.types';
 
-import { SimpleEffect } from '../general/SimpleEffect';
 import ScenarioPanel from '../scenario/ScenarioPanel';
 import type { PageRefetchCallback } from './Page';
-
-const SettingsForm = styled.form`
-  display: block;
-  margin: 1.5rem 0;
-  padding: 0.5rem 0;
-  border-top: 1px solid ${(props) => props.theme.neutralLight};
-  border-bottom: 1px solid ${(props) => props.theme.neutralLight};
-`;
+import { useActionListData } from './useActionListData';
 
 const ActionCount = styled.div`
   padding: ${({ theme }) => theme.spaces.s100} 0;
-  color: ${({ theme }) => theme.themeColors.white};
+  color: ${({ theme }) => theme.themeColors.black};
 
   span {
     margin-left: 1rem;
   }
 `;
 
-const StyledFormControl = styled(FormControl)`
-  width: 100%;
-`;
-
-const StyledFormLabel = styled(FormLabel)`
-  color: inherit;
-  margin-bottom: 0.25rem;
-
-  &.Mui-focused,
-  &.Mui-disabled,
-  &.Mui-error,
-  &.MuiFormLabel-colorPrimary,
-  &.MuiFormLabel-colorSecondary {
-    color: inherit;
-  }
-`;
-
 const ShowLabel = styled(FormLabel)`
   color: ${(p) => p.theme.brandDark};
   margin: 0;
-`;
-
-const StyledSelect = styled(Select)<{ $custom?: boolean }>`
-  .MuiSelect-select {
-    padding: 0.5rem 0.75rem;
-    font-size: 1rem;
-    line-height: 1.5;
-    background-color: ${(props) => props.theme.inputBg};
-  }
-`;
-
-const SortButtons = styled(ToggleButtonGroup)`
-  button {
-    padding-top: 0.4rem;
-    padding-bottom: 0.4rem;
-
-    &.Mui-selected {
-      background-color: ${(props) => props.theme.themeColors.white};
-      svg {
-        fill: ${(props) => props.theme.themeColors.black};
-      }
-    }
-
-    &.Mui-selected:hover {
-      background-color: ${(props) => props.theme.graphColors.grey010};
-    }
-
-    &:hover {
-      background-color: ${(props) => props.theme.graphColors.grey080};
-    }
-
-    svg {
-      fill: ${(props) => props.theme.themeColors.white};
-    }
-  }
-
-  .icon {
-    vertical-align: middle;
-  }
 `;
 
 const ViewSelectorBar = styled.div`
@@ -175,20 +94,14 @@ type ActionListPageProps = {
   refetch: PageRefetchCallback;
 };
 
-function hasGraph(impactResponse: QueryResult<ImpactOverviewsQuery>, graphType: string) {
-  return !!impactResponse.data?.impactOverviews.find(
-    (overview) => overview.graphType === graphType
-  );
-}
-
 function ActionListPage({ page }: ActionListPageProps) {
   const { t } = useTranslation();
   const instance = useInstance();
   const activeGoal = useReactiveVar(activeGoalVar);
+  const yearRange = useReactiveVar(yearRangeVar);
 
-  const impactResp = useQuery<ImpactOverviewsQuery>(GET_IMPACT_OVERVIEWS, {
-    fetchPolicy: 'cache-and-network',
-  });
+  const [listType, setListType] = useState<ViewType>('list');
+  const [ascending, setAscending] = useState(true);
 
   const actionListResp = useQuery<ActionListQuery, ActionListQueryVariables>(GET_ACTION_LIST, {
     variables: {
@@ -196,120 +109,25 @@ function ActionListPage({ page }: ActionListPageProps) {
     },
     fetchPolicy: 'cache-and-network',
   });
-  const error = actionListResp.error || impactResp.error;
-  const { loading: areActionsLoading, previousData } = actionListResp;
-  const yearRange = useReactiveVar(yearRangeVar);
+  const { error, loading: areActionsLoading, previousData } = actionListResp;
 
   const data = actionListResp.data ?? previousData;
-  const hasEfficiency = data ? data.impactOverviews.length > 0 : false;
-  const showReturnOnInvestment = hasGraph(impactResp, 'return_on_investment');
-  const showCostBenefitAnalysis = hasGraph(impactResp, 'cost_benefit');
-  const showSimpleEffect = hasGraph(impactResp, 'simple_effect');
 
-  const sortOptions = getSortOptions(t, hasEfficiency, !!instance.features.showAccumulatedEffects);
-
-  const [listType, setListType] = useState<ViewType>('list');
-  const [ascending, setAscending] = useState(true);
-  const [sortBy, setSortBy] = useState<SortActionsConfig>(
-    sortOptions.find((sortOption) => sortOption.key === page.defaultSortOrder) ?? sortOptions[0]
-  );
   const [activeEfficiency, setActiveEfficiency] = useState<number>(0);
   const [actionGroup, setActionGroup] = useState<string>('ALL_ACTIONS');
 
-  const filteredActions = (data?.actions || []).filter(
-    (action) =>
-      !page.showOnlyMunicipalActions ||
-      (page.showOnlyMunicipalActions && action.decisionLevel === DecisionLevel.Municipality)
-  );
+  const { usableActions, displayedActionsCount, actionGroups, hasEfficiency } = useActionListData({
+    data,
+    showOnlyMunicipalActions: !!page.showOnlyMunicipalActions,
+    activeEfficiency,
+    yearRange,
+    actionGroup,
+  });
 
-  const usableActions: ActionWithEfficiency[] = useMemo(
-    () =>
-      filteredActions
-        .map((act) => {
-          // If we have impact overviews, we augment the actions with the cumulative values
-          /*
-            TODO: ReductionText is unused, intentionally?
-            const reductionText = `(${t('reduction')}, ${t(
-              'accumulated-between'
-            )} ${yearRange[0]}-${yearRange[1]})`;
-          */
-          const out: ActionWithEfficiency = {
-            ...act,
-            impactOnTargetYear:
-              [
-                ...(act.impactMetric?.historicalValues ?? []),
-                ...(act.impactMetric?.forecastValues ?? []),
-              ].find((dataPoint) => dataPoint.year === yearRange[1])?.value ?? 0,
-          };
+  const sortOptions = getSortOptions(t, hasEfficiency, !!instance.features.showAccumulatedEffects);
 
-          const efficiencyType = data?.impactOverviews[activeEfficiency];
-          const efficiencyAction = efficiencyType?.actions.find((a) => a.action.id === act.id);
-
-          if (!efficiencyType || !efficiencyAction) return out;
-
-          out.cumulativeImpact = efficiencyAction.impactValues
-            ? summarizeYearlyValuesBetween(
-                {
-                  historicalValues: efficiencyAction.impactValues
-                    .filter((v): v is NonNullable<typeof v> => v != null)
-                    .map((v) => ({ year: v.year, value: v.value })),
-                  forecastValues: [],
-                },
-                yearRange[0],
-                yearRange[1]
-              )
-            : 0;
-          out.cumulativeCost = efficiencyAction.costValues
-            ? summarizeYearlyValuesBetween(
-                {
-                  historicalValues: efficiencyAction.costValues
-                    .filter((v): v is NonNullable<typeof v> => v != null)
-                    .map((v) => ({ year: v.year, value: v.value })),
-                  forecastValues: [],
-                },
-                yearRange[0],
-                yearRange[1]
-              )
-            : 0;
-          out.unitAdjustmentMultiplier = efficiencyAction.unitAdjustmentMultiplier ?? undefined;
-          if (out.unitAdjustmentMultiplier !== undefined)
-            out.cumulativeEfficiency =
-              (out.cumulativeCost / Math.abs(out.cumulativeImpact)) * out.unitAdjustmentMultiplier;
-
-          /*
-            TODO: Is invertImpact available somewhere in api?
-            cumulativeImpactName: `${efficiencyType?.effectNode?.name} ${
-              data.impactOverviews[activeEfficiency]?.invertImpact ? reductionText : ''
-            }`,
-          */
-          const efficiencyProps: Partial<ActionWithEfficiency> = {
-            cumulativeImpactId: efficiencyType?.effectNode?.id,
-            cumulativeImpactUnit: efficiencyType?.effectUnit?.htmlShort,
-            cumulativeImpactName: `${efficiencyType?.effectNode?.name}`,
-            cumulativeCostUnit: efficiencyType?.costUnit?.htmlShort,
-            cumulativeCostName: efficiencyType?.costNode?.name,
-            cumulativeEfficiencyUnit: efficiencyType?.indicatorUnit.htmlShort,
-            cumulativeEfficiencyName: efficiencyType?.label,
-            efficiencyCap: efficiencyType?.plotLimitForIndicator ?? undefined,
-          };
-          Object.assign(out, efficiencyProps);
-          return out;
-        })
-        .filter((action) => actionGroup === 'ALL_ACTIONS' || actionGroup === action.group?.id),
-    [data, actionGroup, activeEfficiency, yearRange, filteredActions]
-  );
-
-  const displayedActionsCount = useMemo(() => {
-    const hasAnyGroup = usableActions.some((a) => a.group);
-    return hasAnyGroup ? usableActions.filter((a) => a.group).length : usableActions.length;
-  }, [usableActions]);
-
-  const actionGroups = filteredActions.reduce(
-    (groups: NonNullable<ActionWithEfficiency['group']>[], action) =>
-      !action.group || groups.find((group) => group.id === action.group?.id)
-        ? groups
-        : [...groups, action.group],
-    []
+  const [sortBy, setSortBy] = useState<SortActionsConfig>(
+    sortOptions.find((sortOption) => sortOption.key === page.defaultSortOrder) ?? sortOptions[0]
   );
 
   const handleChangeSort = (sortBy: SortActionsBy) => {
@@ -326,9 +144,7 @@ function ActionListPage({ page }: ActionListPageProps) {
 
   const viewOptions: ViewOption[] = [
     getViewOption('list', t('actions-as-list'), 'list'),
-    ...(hasEfficiency || showCostBenefitAnalysis || showReturnOnInvestment || showSimpleEffect
-      ? [getViewOption('graph', t('actions-as-graph'), 'chartColumn')]
-      : []),
+    ...(hasEfficiency ? [getViewOption('graph', t('actions-as-graph'), 'chartColumn')] : []),
   ];
   const hasMultipleViews = viewOptions.length > 1;
 
@@ -340,10 +156,6 @@ function ActionListPage({ page }: ActionListPageProps) {
     );
   }
 
-  if (!data) {
-    return <ContentLoader fullPage />;
-  }
-
   return (
     <>
       <PageHero
@@ -351,111 +163,39 @@ function ActionListPage({ page }: ActionListPageProps) {
         leadDescription={page.actionListLeadParagraph ?? undefined}
       >
         <ScenarioPanel />
-        <SettingsForm className="text-light mt-4">
-          <Grid container spacing={2}>
-            {hasEfficiency && (
-              <Grid size={{ md: 4 }} sx={{ display: 'flex' }}>
-                <StyledFormControl>
-                  <StyledFormLabel id="impact-label" htmlFor="impact">
-                    {t('actions-impact-on')}
-                  </StyledFormLabel>
-                  <StyledSelect
-                    id="impact"
-                    labelId="impact-label"
-                    value={activeEfficiency}
-                    onChange={(e) => setActiveEfficiency(Number(e.target.value))}
-                    size="small"
-                    MenuProps={{ disablePortal: true }}
-                  >
-                    {data.impactOverviews.map((impactGroup, indx) => (
-                      <MenuItem value={indx} key={indx}>
-                        {impactGroup.label}
-                      </MenuItem>
-                    ))}
-                  </StyledSelect>
-                </StyledFormControl>
-              </Grid>
-            )}
 
-            {actionGroups.length > 1 && (
-              <Grid size={{ md: 4 }} sx={{ display: 'flex' }}>
-                <StyledFormControl>
-                  <StyledFormLabel id="type-label" htmlFor="type">
-                    {t('actions-group-type')}
-                  </StyledFormLabel>
-                  <StyledSelect
-                    id="type"
-                    labelId="type-label"
-                    value={actionGroup}
-                    onChange={(e) => setActionGroup(e.target.value as string)}
-                    size="small"
-                    MenuProps={{ container: () => document.getElementById('main')! }}
-                  >
-                    <MenuItem value="ALL_ACTIONS">{t('action-groups-all')}</MenuItem>
-                    {actionGroups.map((group) => (
-                      <MenuItem value={group.id} key={group.id}>
-                        {group.name}
-                      </MenuItem>
-                    ))}
-                  </StyledSelect>
-                </StyledFormControl>
-              </Grid>
-            )}
-
-            <Grid size={{ md: 4 }} sx={{ display: 'flex' }}>
-              <Box sx={{ display: 'flex', alignItems: 'flex-end', mr: 1.5 }}>
-                <StyledFormControl>
-                  <StyledFormLabel id="sort-label" htmlFor="sort">
-                    {t('actions-sort-by')}
-                  </StyledFormLabel>
-                  <StyledSelect
-                    id="sort"
-                    labelId="sort-label"
-                    value={sortBy.key}
-                    onChange={(e) => handleChangeSort(e.target.value as SortActionsBy)}
-                    size="small"
-                    MenuProps={{ container: () => document.getElementById('main')! }}
-                  >
-                    {sortOptions
-                      .filter((opt) => !opt.isHidden)
-                      .map((opt) => (
-                        <MenuItem key={opt.key} value={opt.key}>
-                          {opt.label}
-                        </MenuItem>
-                      ))}
-                  </StyledSelect>
-                </StyledFormControl>
-              </Box>
-
-              <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                <StyledFormControl>
-                  <StyledFormLabel>{t('sort-direction')}</StyledFormLabel>
-                  <SortButtons
-                    value={ascending ? 'asc' : 'desc'}
-                    exclusive
-                    onChange={handleSortDirectionChange}
-                    aria-label={t('sort-direction')}
-                  >
-                    <ToggleButton value="asc" aria-label={t('sort-ascending')}>
-                      <Icon name="arrowUpWideShort" width="1.5rem" height="1.5rem" />
-                    </ToggleButton>
-                    <ToggleButton value="desc" aria-label={t('sort-descending')}>
-                      <Icon name="arrowDownShortWide" width="1.5rem" height="1.5rem" />
-                    </ToggleButton>
-                  </SortButtons>
-                </StyledFormControl>
-              </Box>
-            </Grid>
-          </Grid>
-        </SettingsForm>
-        <ActionCount>
-          <div>{t('actions-count', { count: displayedActionsCount })}</div>
-        </ActionCount>
+        {data && (
+          <ActionListFilters
+            hasEfficiency={hasEfficiency}
+            data={data}
+            activeEfficiency={activeEfficiency}
+            setActiveEfficiency={setActiveEfficiency}
+            actionGroups={actionGroups}
+            actionGroup={actionGroup}
+            setActionGroup={setActionGroup}
+            sortBy={sortBy}
+            sortOptions={sortOptions}
+            handleChangeSort={handleChangeSort}
+            ascending={ascending}
+            handleSortDirectionChange={handleSortDirectionChange}
+          />
+        )}
       </PageHero>
 
       {hasMultipleViews && (
         <ViewSelectorBar className="text-light">
-          <Container fixed maxWidth="xl">
+          <Container
+            fixed
+            maxWidth="xl"
+            sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+          >
+            <ActionCount>
+              {areActionsLoading ? (
+                <span>{t('loading')}</span>
+              ) : (
+                <span>{t('actions-count', { count: displayedActionsCount })}</span>
+              )}
+            </ActionCount>
             <Box sx={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center', gap: 1 }}>
               <ShowLabel
                 id="view-select-label"
@@ -509,48 +249,15 @@ function ActionListPage({ page }: ActionListPageProps) {
             }}
           />
         ) : (
-          (() => {
-            const selectedOverview = impactResp.data?.impactOverviews[activeEfficiency];
-            const graphType = selectedOverview?.graphType;
-
-            switch (graphType) {
-              case 'cost_efficiency':
-                return (
-                  <ActionsMac
-                    id="efficiency-view"
-                    actions={usableActions}
-                    impactOverviews={selectedOverview}
-                    t={t}
-                    actionGroups={data.instance.actionGroups}
-                    sortBy={sortBy.sortKey}
-                    sortAscending={ascending}
-                    refetching={areActionsLoading}
-                  />
-                );
-              case 'cost_benefit':
-                return (
-                  <CostBenefitAnalysis data={selectedOverview} isLoading={impactResp.loading} />
-                );
-              case 'return_on_investment':
-                return (
-                  <ReturnOnInvestment data={selectedOverview} isLoading={impactResp.loading} />
-                );
-              case 'simple_effect':
-                return <SimpleEffect data={selectedOverview} isLoading={impactResp.loading} />;
-              default:
-                return (
-                  <ActionsComparison
-                    id="comparison-view"
-                    actions={usableActions}
-                    actionGroups={data.instance.actionGroups}
-                    sortBy={sortBy.sortKey}
-                    sortAscending={ascending}
-                    refetching={areActionsLoading}
-                    displayYears={yearRange}
-                  />
-                );
-            }
-          })()
+          <ActionListGraphView
+            usableActions={usableActions}
+            activeEfficiency={activeEfficiency}
+            instanceActionGroups={data?.instance.actionGroups ?? []}
+            sortBy={sortBy}
+            sortAscending={ascending}
+            refetching={areActionsLoading}
+            yearRange={yearRange}
+          />
         )}
       </Container>
     </>
