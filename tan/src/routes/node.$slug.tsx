2bede8a814969a93c9f8a76ae5cf335a7cabfa12
Tero Tikkanen <tero.tikkanen@gmail.com>
2026-05-06 12:34:22 +0300

    Remove tan dir

--- tan/src/routes/node.$slug.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-import NodePage from '@/pages/node/[slug]';
-
-export const Route = createFileRoute('/node/$slug')({ component: NodePage });
