488284254704ce6a66859c93f7ac444301c2d610
Tero Tikkanen <tero.tikkanen@gmail.com>
2026-05-06 17:47:11 +0300

    Update to latest kausal_common (#268)
    * Update to latest kausal_common
    
    * Regenerate pnpm-lock
    
    * Remove tan dir

--- tan/src/routes/actions.$slug.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-import ActionPage from '@/pages/actions/[slug]';
-
-export const Route = createFileRoute('/actions/$slug')({ component: ActionPage });
--- tan/src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-import Page from '@/components/pages/Page';
-
-export const Route = createFileRoute('/')({ component: HomePage });
-
-function HomePage() {
-  return <Page path="/" />;
-}
