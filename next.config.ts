5349efc36c76465954f5737c9da2d8e30fc38873
Juha Yrjölä <juha.yrjola@kausal.tech>
2026-07-31 13:52:38 +0300

    Configure nextjs `allowedDevOrigins` for local dev

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ import type { Options as SassOptions } from 'sass';
 
 import { getNextConfig } from './kausal_common/configs/common-next-config.ts';
 import { wrapWithSentryConfig } from './kausal_common/configs/sentry-next-config.ts';
+import { getWildcardDomains } from './kausal_common/src/env/runtime.ts';
+import { isLocalDev } from './kausal_common/src/env/static.ts';
 import { initializeThemes } from './kausal_common/src/themes/next-config.mjs';
 
 process.env.NEXT_TELEMETRY_DISABLED = '1';
@@ -24,6 +26,10 @@ let nextConfig: NextConfig = {
     ],
   } satisfies SassOptions<'sync'>,
 };
+
+if (isLocalDev) {
+  nextConfig.allowedDevOrigins = getWildcardDomains().map((domain) => `*.${domain}`);
+}
 
 nextConfig = wrapWithSentryConfig(nextConfig);
 nextConfig = withNextIntl('./src/config/i18n.ts')(nextConfig);
